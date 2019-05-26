e4e4c011d4cf3f1d685e37dedbda453fbd7ff218
Michael Schade <github@mschade.me>
2019-05-26 00:28:45 -0700

    save auth credentials locally

--- remarkable.js
+++ remarkable.js
@@ -1,21 +1,71 @@
 const Remarkable = {};
 
 (async function() {
-const RemarkableConfig = {
+
+let baseAuthUrl = 'https://my.remarkable.com/token/json/2';
+let baseDocsUrl = 'https://document-storage-production-dot-remarkable-production.appspot.com';
+
+const Config = {
   urls: {
-    base: 'https://document-storage-production-dot-remarkable-production.appspot.com',
-    uploadRequest: '/document-storage/json/2/upload/request',
-    uploadStatus: '/document-storage/json/2/upload/update-status',
+    uploadRequest: `${baseDocsUrl}/document-storage/json/2/upload/request`,
+    uploadStatus: `${baseDocsUrl}/document-storage/json/2/upload/update-status`,
+    newDevice: `${baseAuthUrl}/device/new`,
+    refreshToken: `${baseAuthUrl}/user/new`,
   }
 };
-RemarkableConfig.secrets = await (await fetch(chrome.runtime.getURL('data/remarkable.json'))).json();
+const Auth = await chrome.storage.local.get(['deviceToken'])
+
+Remarkable.isDeviceRegistered = function() {
+  return !!Auth.deviceToken;
+}
+
+// Register device using code at https://my.remarkable.com/generator-device
+Remarkable.registerDevice = async function(code) {
+  if (Remarkable.isDeviceRegistered()) {
+    throw new Error("Device already registered")
+  }
+  let request = await fetch(Config.urls.newDevice, {
+    method: 'POST',
+    body: JSON.stringify({
+      'code': code,
+      'deviceDesc': 'desktop-linux',
+      'deviceId': uuid4()
+    })
+  });
+  if (!request.ok) {
+    throw new Error(await request.text());
+  }
+  let token = await request.text();
+  chrome.storage.local.set({
+    'deviceToken': token
+  })
+  Auth.deviceToken = deviceToken;
+}
+
+// Refresh the Bearer token before using.
+// Should probably cache this, but let's just refresh it every use for now
+async function bearerToken() {
+  if (!Remarkable.isDeviceRegistered()) {
+    throw new Error("Must register device first. Call `Remarkable.registerDevice` with a one-time code from https://my.remarkable.com/generator-device to get started.");
+  }
+  let request = await fetch(Config.urls.refreshToken, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${Auth.deviceToken}`,
+    }
+  });
+  if (!request.ok) {
+    throw new Error(await request.text());
+  }
+  return request.text();
+}
 
 // Make a request to upload a new document
 Remarkable.uploadRequest = async function() {
-  let request = await fetch(RemarkableConfig.urls.base + RemarkableConfig.urls.uploadRequest, {
+  let request = await fetch(Config.urls.uploadRequest, {
     method: 'PUT',
     headers: {
-      'Authorization': `Bearer ${RemarkableConfig.secrets.userToken}`,
+      'Authorization': `Bearer ${await bearerToken()}`,
     },
     body: JSON.stringify([{
       'ID': uuid4(),
@@ -23,6 +73,9 @@ Remarkable.uploadRequest = async function() {
       'Version': 1
     }])
   });
+  if (!request.ok) {
+    throw new Error(await request.text());
+  }
   let payload = await request.json();
   return payload[0];
 }
@@ -38,10 +91,10 @@ Remarkable.uploadDocument = async function(endpoint, zip) {
 
 // Update document properties. Required to make document visible after uploading.
 Remarkable.updateDocument = async function(id, name, parent='') {
-  let request = await fetch(RemarkableConfig.urls.base + RemarkableConfig.urls.uploadStatus, {
+  let request = await fetch(Config.urls.uploadStatus, {
     method: 'PUT',
     headers: {
-      'Authorization': `Bearer ${RemarkableConfig.secrets.userToken}`,
+      'Authorization': `Bearer ${await bearerToken()}`,
     },
     body: JSON.stringify([{
       'ID': id,
@@ -51,6 +104,9 @@ Remarkable.updateDocument = async function(id, name, parent='') {
       'Version': 1
     }])
   });
+  if (!request.ok) {
+    throw new Error(await request.text());
+  }
   let payload = await request.json();
   return payload[0];
 }
