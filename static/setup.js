ed8fe907b5aa928043f2988730b6acd567e594c2
Michael Schade <github@mschade.me>
2019-07-17 11:24:48 -0700

    page tweaks

--- static/setup.js
+++ static/setup.js
@@ -1,3 +1,9 @@
+$(document).ready(async function() {
+  if (await Remarkable.isDeviceRegistered()) {
+    console.log('registered');
+  }
+});
+
 $(document).ready(function() {
   $('.sign-in form input[name=code]').on('input', function() {
     $(this).next('button').attr('disabled', $(this).val().length !== 8);
@@ -9,12 +15,13 @@ $(document).ready(function() {
     try {
       let code = $('form input[name=code]').val();
       $(this).parents('.container').removeClass('error');
+      $('#status').removeClass('error').text('Authenticating with reMarkable\u2026');
       $('form > *').attr('disabled', 'disabled');
       await Remarkable.registerDevice(code);
       $(this).parents('.container').addClass('hide').next().addClass('show');
       $('.gradient-success').addClass('show');
     } catch(err) {
-      $('#error').text(err);
+      $('#status').addClass('error').text(err);
       $(this).parents('.container').addClass('error');
       $('form > *').attr('disabled', false);
     }
